99ba0d07b2da543b710d323bac6a33508e7ce32d
Han Wang <hanforwork896@gmail.com>
2025-03-23 21:12:03 +0800

    feat: improve day3

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useLocalStorage } from "@/hooks";
-
-// TODO: rm from localStorage
+import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash';
+import { v4 as uuidv4 } from 'uuid';
 
 function TodoList() {
   const [newTodo, setNewTodo] = useState("");
@@ -9,22 +9,29 @@ function TodoList() {
 
   const handleToggleTodo = (todo) => {
     const updatedTodos = todos.map((t) =>
-      t.id === todo.id ? { ...t, status: !t.status } : t
+      t.id === todo.id
+        ? { ...t, status: !t.status }
+        : t
     );
     setTodos(updatedTodos);
-  }
+  };
 
   const handleAddTodo = (e) => {
     e.preventDefault();
 
     const newTodoItem = {
-      id: Date.now(),
+      id: uuidv4(),
       text: newTodo,
       status: false,
     }
     setTodos([...todos, newTodoItem]);
     setNewTodo("");
-  }
+  };
+
+  const handleDeleteTodo = (todo) => {
+    const target = todos.filter(t => t.id !== todo.id);
+    setTodos(target);
+  };
 
   return(
     <div>
@@ -46,6 +53,7 @@ function TodoList() {
               onChange={() => handleToggleTodo(todo)}
             />
             <span className={todo.status ? "line-through" : ""}>{todo.text}</span>
+            <RestoreFromTrashIcon className="cursor-pointer hover:opacity-50 ml-4" onClick={() => handleDeleteTodo(todo)} />
           </li>
         ))}
       </ul>
