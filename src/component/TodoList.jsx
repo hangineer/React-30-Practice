bb3fe2f46c83ef2b61964a6099e295befce2f700
Han Wang <hanforwork896@gmail.com>
2025-05-27 22:13:30 +0800

    fix: code formatted

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useLocalStorage } from "@/hooks";
-import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash';
-import { v4 as uuidv4 } from 'uuid';
+import RestoreFromTrashIcon from "@mui/icons-material/RestoreFromTrash";
+import { v4 as uuidv4 } from "uuid";
 
 function TodoList() {
   const [newTodo, setNewTodo] = useState("");
